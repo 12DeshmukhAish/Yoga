02b7c7bfcdab6732bfb092385637bcc1572b988e
12DeshmukhAish <113546938+12DeshmukhAish@users.noreply.github.com>
2023-10-27 20:09:41 +0530

    AdminPaneladded

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 3000; // You can change this to the desired port.
-const mongoURI = 'mongodb+srv://maneprathamesh019:maneprathamesh019@cluster0.6tn2owo.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'; // Replace with your MongoDB connection URI.
+// const mongoURI =
 
 // MongoDB connection setup
+const mongoURI =
+  "mongodb+srv://deshmukhaishwarya484:deshmukhaishwarya484@cluster0.c2qnvez.mongodb.net/?retryWrites=true&w=majority"; // Replace with your MongoDB connection URI.
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const ContactSchema = new mongoose.Schema({
   name: String,
   email: String,
   subject: String,
-  message: String
+  message: String,
 });
 
-const Contact = mongoose.model('Contact', ContactSchema);
+const Contact = mongoose.model("Contact", ContactSchema);
 
 // Middleware to parse JSON and url-encoded data
 
@@ -33,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 // Route for handling form submissions
-app.post('/submit', (req, res) => {
+app.post("/submit", (req, res) => {
   const { name, email, subject, message } = req.body;
   console.log(name);
 
@@ -42,21 +44,21 @@ app.post('/submit', (req, res) => {
     name,
     email,
     subject,
-    message
+    message,
   });
 
   // Save the record to the database
-  
-  newContact.save()
+
+  newContact
+    .save()
     .then(() => {
       sendEmail(name, email);
-      res.status(200).send('Contact information saved successfully.');
+      res.status(200).send("Contact information saved successfully.");
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error saving contact information.');
+      res.status(500).send("Error saving contact information.");
     });
-
 });
 
 app.listen(port, () => {
@@ -74,13 +76,101 @@ async function sendEmail(name, email) {
       },
     });
 
-// Email configuration
+    // Email configuration
 
     const mailOptions = {
       from: "mentaldivine.com",
       to: email,
-      subject: "Contact request recived",
-      text: "Thank " + name + " for connecting with us",
+      subject: "Contact request received",
+      html: `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Thank You for Connecting Mental Divine</title>
+        <style>
+            /* Reset some default styles */
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f2f2f2;
+                margin: 0;
+                padding: 0;
+            }
+
+            /* Email content styles */
+            .email-container {
+                max-width: 600px;
+                margin: 0 auto;
+                background-color: #fff;
+                padding: 20px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                border-radius: 5px;
+            }
+
+            .header {
+                background-color: #007BFF;
+                color: #fff;
+                padding: 20px;
+                border-radius: 5px 5px 0 0;
+            }
+
+            .header h1 {
+                font-size: 24px;
+                margin: 0;
+            }
+
+            .email-content {
+                padding: 20px;
+            }
+
+            .email-content p {
+                font-size: 16px;
+                margin: 5px 0;
+            }
+
+            .footer {
+                background-color: #007BFF;
+                color: #fff;
+                padding: 10px;
+                text-align: center;
+                border-radius: 0 0 5px 5px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="email-container">
+        <div class="header">
+            <h1>Thank You for Connecting Mental Divine</h1>
+        </div>
+        
+        <div class="email-content">
+         <p>"Dear" ${name},</p>           
+            <p>We want to express our gratitude for reaching out to Mental Divine. Your mental well-being is important to us, and we are here to provide you with the support and guidance you need to manage stress and improve your mental health.</p>
+            
+            <p>At Mental Divine, we offer a wide range of resources and services to help you reduce stress and find solutions to your mental health concerns. You can explore:</p>
+            
+            <ul>
+                <li>Yoga sessions and courses</li>
+                <li>Stress reduction techniques</li>
+                <li>Mental health articles and advice</li>
+                <li>Community support</li>
+            </ul>
+            
+            <p>We encourage you to visit our website regularly and take advantage of the valuable information and resources we provide. Your well-being is our top priority, and we are here to support you on your journey to better mental health.</p>
+            
+            <p>If you have any further questions or need assistance, please do not hesitate to contact us at [Your Contact Information].</p>
+            
+            <p>Thank you for choosing Mental Divine as your partner in mental health and well-being. We look forward to accompanying you on your path to a healthier, happier life.</p>
+        </div>
+    
+        <div class="footer">
+            <p>@mentaldivine</p>
+            <p>123 Street, Manish Nagar ,Pune ,Maharashtra</p>
+        </div>
+    </div>
+    </body>
+    </html>
+  `,
     };
 
     // Send the email
